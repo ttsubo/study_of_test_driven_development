c1e5b4b290553c8671d09286f46d6706bb85eda7
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2020-01-04 08:20:00 +0900

    第4章: 意図を語るテスト (Privacy)

--- python/example/dollar.py
+++ python/example/dollar.py
@@ -1,9 +1,9 @@
 class Dollar:
     def __init__(self, amount):  
-        self.amount = amount
+        self.__amount = amount
 
     def __eq__(self, other):
-        return self.amount == other.amount
+        return self.__amount == other.__amount
 
     def times(self, multiplier):                                                    
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
--- python/tests/test_money.py
+++ python/tests/test_money.py
@@ -4,10 +4,8 @@ from example.dollar import Dollar
 class MoneyTest(TestCase):
     def testMultiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(Dollar(10), five.times(2))
+        self.assertEqual(Dollar(15), five.times(3))
 
     def testEquality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
