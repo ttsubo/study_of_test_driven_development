90e45e2978a37687890e9820d3181c9583d9fd8b
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2020-01-04 07:51:55 +0900

    第2章: 明白な実装 (Degenerate Objects)

--- python/tests/test_money.py
+++ python/tests/test_money.py
@@ -4,5 +4,7 @@ from example.dollar import Dollar
 class MoneyTest(TestCase):
     def testMultiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
